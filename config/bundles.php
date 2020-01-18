e0d79d75aac3f5061718da33e64deaae1550a87f
programarivm <info@programarivm.com>
2020-01-18 19:45:55 +0000

    composer require --dev dama/doctrine-test-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
