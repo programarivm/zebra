42b8cd93bffbfb35370c2e8241715c36cdf87b6a
programarivm <info@programarivm.com>
2020-01-20 09:45:50 +0000

    user fixtures are successfully loaded (20 fake users are created with fzaninotto/faker)

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,28 +5,34 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
     private $encoder;
 
+    private $faker;
+
     public function __construct(UserPasswordEncoderInterface $encoder)
     {
         $this->encoder = $encoder;
+        $this->faker = Factory::create();
+        $this->faker->addProvider(new \Faker\Provider\Internet($this->faker));
     }
 
     public function load(ObjectManager $manager)
     {
-        $user = new User();
-        $user->setUsername('bob')
-            ->setEmail('bob@gmail.com')
-            ->setPassword($this->encoder->encodePassword(
-                $user,
-                'password'
-            ));
-
-        $manager->persist($user);
+        for ($i = 0; $i < 20; $i++) {
+            $user = new User();
+            $user->setUsername($this->faker->username)
+                ->setEmail($this->faker->email)
+                ->setPassword($this->encoder->encodePassword(
+                    $user,
+                    $this->faker->password
+                ));
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
