4cb8775584bbcd4110fc1f30a866d996f9ef8ae3
programarivm <info@programarivm.com>
2020-01-26 10:01:44 +0000

    TDDing the User entity. validate_bob() passes.

--- tests/unit/Entity/UserTest.php
+++ tests/unit/Entity/UserTest.php
@@ -8,21 +8,46 @@ use Symfony\Component\Validator\Validation;
 
 class UserTest extends TestCase
 {
+    protected static $validator;
+
+    public static function setUpBeforeClass()
+    {
+        self::$validator = Validation::createValidatorBuilder()
+                                ->addYamlMapping('config/validator/validation.yaml')
+                                ->getValidator();
+    }
+
     /**
      * @test
      */
     public function validate_falsy()
     {
-        $validator = Validation::createValidatorBuilder()
-                        ->addYamlMapping('config/validator/validation.yaml')
-                        ->getValidator();
-
-        $errors = (new User())->validate($validator);
+        $errors = (new User())->validate(self::$validator);
 
         $expected = [
             "The user's username cannot be empty.",
             "The user's email cannot be empty.",
-            "The user's password cannot be empty.",
+        ];
+
+        $this->assertEquals($expected, $errors);
+    }
+
+    /**
+     * @test
+     */
+    public function validate_bob()
+    {
+        $props = [
+            'username' => 'bob',
+            'email' => 'bob',
+        ];
+
+        $errors = (new User())
+                    ->setProps($props)
+                    ->validate(self::$validator);
+
+        $expected = [
+            "The user's email is not a valid one.",
         ];
 
         $this->assertEquals($expected, $errors);
