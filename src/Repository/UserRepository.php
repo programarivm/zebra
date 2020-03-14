dd5f0e7797cd2212110ff7c901fe320adc496edd
programarivm <info@programarivm.com>
2020-03-14 12:42:57 +0000

    deleteAll() is implemented in UserRepository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -19,32 +19,12 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    // /**
-    //  * @return User[] Returns an array of User objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function deleteAll()
     {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+        $dql = 'DELETE FROM App\Entity\User';
+        $query = $this->getEntityManager()
+                    ->createQuery($dql);
 
-    /*
-    public function findOneBySomeField($value): ?User
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return $query->execute();
     }
-    */
 }
