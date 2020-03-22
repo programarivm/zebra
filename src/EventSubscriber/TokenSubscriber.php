0e3d11f0f47986490371fa06b44e4ab02fa83e77
programarivm <info@programarivm.com>
2020-03-22 17:05:28 +0000

    controller tests fail since TokenSubscriber has been added

---
+++ src/EventSubscriber/TokenSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Controller\TokenAuthenticatedController;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class TokenSubscriber implements EventSubscriberInterface
+{
+    public function onKernelController(ControllerEvent $event)
+    {
+        $controller = $event->getController();
+
+        // when a controller class defines multiple action methods, the controller
+        // is returned as [$controllerInstance, 'methodName']
+        if (is_array($controller)) {
+            $controller = $controller[0];
+        }
+
+        // TODO
+        // Authenticate and authorize (ACL)
+        if ($controller instanceof TokenAuthenticatedController) {
+            $token = $event->getRequest()->query->get('token');
+            if (empty($token)) {
+                throw new AccessDeniedHttpException('This action needs a valid token!');
+            }
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::CONTROLLER => 'onKernelController',
+        ];
+    }
+}
